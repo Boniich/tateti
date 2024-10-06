eeff07f96bf19858249a83a559b3b162a7ad686a
Boniich <ezequieldbo25@gmail.com>
2024-10-06 14:08:08 -0300

    fix: fix error with cin when a wrong data type is typed

--- Tateti/game/view/SelectMarkView.cpp
+++ Tateti/game/view/SelectMarkView.cpp
@@ -12,28 +12,41 @@ void selectMarkView(Player& player, Player& npc) {
         std::cout << "1- X" << std::endl;
         std::cout << "2- O" << std::endl;
         std::cout << "Que marca deseas usar?" << std::endl;
-        std::cin >> opcion;
+
+        if (std::cin >> opcion) {
+
+            switch (opcion)
+            {
+            case 1:
+                system("cls");
+                playerSelection = 'X';
+                npcSelection = 'O';
+                selectMark(player, playerSelection, npc, npcSelection);
+
+                break;
+            case 2:
+                system("cls");
+                playerSelection = 'O';
+                npcSelection = 'X';
+                selectMark(player, playerSelection, npc, npcSelection);
+                break;
+            default:
+                system("cls");
+                std::cout << "---------------------------------------" << std::endl;
+                std::cout << "La opcion ingresada no es valida" << std::endl;
+                std::cout << "---------------------------------------" << std::endl;
+                break;
+            }
 
 
-        switch (opcion)
-        {
-        case 1:
+        }
+        else {
             system("cls");
-            playerSelection = 'X';
-            npcSelection = 'O';
-            selectMark(player, playerSelection, npc, npcSelection);
-
-            break;
-        case 2:
-            system("cls");
-            playerSelection = 'O';
-            npcSelection = 'X';
-            selectMark(player, playerSelection, npc, npcSelection);
-            break;
-        default:
-            system("cls");
-            std::cout << "La opcion ingresada no es valida" << std::endl;
-            break;
+            std::cout << "---------------------------------------" << std::endl;
+            std::cout << "Error: se ingreso un caracter no valido" << std::endl;
+            std::cout << "---------------------------------------" << std::endl;
+            std::cin.clear();
+            std::cin.ignore();
         }
 
     } while (opcion != 1 && opcion != 2);
