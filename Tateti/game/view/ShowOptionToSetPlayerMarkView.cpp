a132704d0d954d4577ddb576258eef9448329bf0
Boniich <ezequieldbo25@gmail.com>
2024-10-09 22:06:16 -0300

    feat: change how code handle to avoid return pointers and other stuffs

--- Tateti/game/view/ShowOptionToSetPlayerMarkView.cpp
+++ Tateti/game/view/ShowOptionToSetPlayerMarkView.cpp
@@ -1,23 +1,63 @@
 #include "ShowOptionToSetPlayerMarkView.h"
 
 
+void setPlayerMarkView(char tableGame[3][3],const char mark) {
+	
+	int seleccion;
+	bool exit = false;
+	int amountSpaceFree = 0;
+	char(*ptr)[2] = showOptionToSetPlayerMark(tableGame, amountSpaceFree);
+	static char positionToSetMark[3];
+
+	do {
+
+
+		std::cout << "Seleciona una opcion: ";
+
+		if (std::cin >> seleccion) {
+
+			if (seleccion >= 1 && seleccion <= amountSpaceFree) {
+				std::cout << "La opcion selecciona es: " << ptr[seleccion - 1][0] << ptr[seleccion - 1][1];
+				positionToSetMark[0] = ptr[seleccion - 1][0];
+				positionToSetMark[1] = ptr[seleccion - 1][1];
+				exit = true;
+			}
+			else {
+				std::cout << "---------------------------------------" << std::endl;
+				std::cout << "La opcion ingresada no es valida" << std::endl;
+				std::cout << "---------------------------------------" << std::endl;
+			}
+
+		}
+		else {
+			std::cout << "---------------------------------------" << std::endl;
+			std::cout << "Error: se ingreso un caracter no valido" << std::endl;
+			std::cout << "---------------------------------------" << std::endl;
+			std::cin.clear();
+			std::cin.ignore();
+		}
+
+	} while (!exit);
+
+
+	setPlayerMark(tableGame, positionToSetMark, mark);
+	
+}
+
 
 void ShowOptionToSetPlayerMarkView(char tableGame[3][3]) {
 
 	int amountSpaceFree = 0;
 	char(*ptr)[2] = showOptionToSetPlayerMark(tableGame, amountSpaceFree);
 
-	std::cout << "Lugares para poner tu marca: ";
+	std::cout << " ---- Lugares para poner tu marca ----"<<std::endl;
 	for (int i = 0; i < amountSpaceFree; i++) {
+		std::cout << (i + 1)<< "- ";
 		for (int e = 0; e < 2; e++) {
 			std::cout << ptr[i][e];
 		}
-		std::cout << " ; ";
+		std::cout << "" << std::endl;
 	}
 
 	std::cout << "" << std::endl;
-
-	std::cout << "Selecciona una opcion: ";
-
-
 }
--- Tateti/game/view/ShowOptionToSetPlayerMarkView.h
+++ Tateti/game/view/ShowOptionToSetPlayerMarkView.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
 #include "../model/ShowOptionToSetPlayerMark.h"
+#include "../model/SetPlayerMark.h"
 
 void ShowOptionToSetPlayerMarkView(char tableGame[3][3]);
+void setPlayerMarkView(char tableGame[3][3],const char mark);
