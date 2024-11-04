2bc8636008ac5cdf4c7f9dd97cb723d8912a846f
Boniich <ezequieldbo25@gmail.com>
2024-11-04 11:25:02 -0300

    feat: add check in the menu when someone enter a wrong character

--- Tateti/game/menu/Menu.cpp
+++ Tateti/game/menu/Menu.cpp
@@ -13,26 +13,36 @@ void menu(char tableGame[3][3], Player player, Player npc) {
         std::cout << "3- Salir" << std::endl;
         std::cin >> opcion;
 
-
-        switch (opcion)
-        {
-        case 1:
-            system("cls");
-            playGame(tableGame, player, npc);
-            break;
-        case 2:
-            system("cls");
-            showPointers(player, npc);
-            std::cout << "" << std::endl;
-            break;
-        case 3:
-            std::cout << "Adios! hasta la proxima!" << std::endl;
-            endGame = true;
-            break;
-        default:
-            std::cout << "La opcion no es valida. Vuelve a intentarlo" << std::endl;
-            break;
+        if (std::cin >> opcion) {
+            switch (opcion)
+            {
+            case 1:
+                system("cls");
+                playGame(tableGame, player, npc);
+                break;
+            case 2:
+                system("cls");
+                showPointers(player, npc);
+                std::cout << "" << std::endl;
+                break;
+            case 3:
+                std::cout << "Adios! hasta la proxima!" << std::endl;
+                endGame = true;
+                break;
+            default:
+                std::cout << "La opcion no es valida. Vuelve a intentarlo" << std::endl;
+                break;
+            }
         }
+        else {
+            system("cls");
+            std::cout << "---------------------------------------" << std::endl;
+            std::cout << "Error: se ingreso un caracter no valido" << std::endl;
+            std::cout << "---------------------------------------" << std::endl;
+            std::cin.clear();
+            std::cin.ignore();
+        }
+       
 
 
     } while (!endGame);
