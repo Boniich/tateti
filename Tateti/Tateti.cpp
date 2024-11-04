476ea6fb7d377a9c65a45804ea56ec0721cb2362
Boniich <ezequieldbo25@gmail.com>
2024-11-04 10:02:38 -0300

    feat:update main

--- Tateti/Tateti.cpp
+++ Tateti/Tateti.cpp
@@ -1,10 +1,7 @@
 ﻿#include <iostream>
 #include <windows.h>
 #include "game/view/SelectMarkView.h"
-#include "game/view/ShowTableGameView.h"
-#include "game/view/ShowOptionToSetPlayerMarkView.h"
-#include "game/view/SetNpcMarkView.h"
-#include "game/view/DeterminateWinnerView.h"
+#include "game/view/PlayGame.h"
 
 
 
@@ -16,8 +13,8 @@ int main()
     SetConsoleOutputCP(CP_UTF8);
 
 
-    bool gameOver = false;
-    bool isThereWinner = false;
+    bool endGame = false;
+    int opcion;
     Player player;
     Player npc;
     char tableGame[3][3] = { {' ',' ',' '}
@@ -29,34 +26,32 @@ int main()
     std::cout << "El player tiene la marca: " << player.mark << std::endl;
     std::cout << "El npc tiene la marca: " << npc.mark << std::endl;
 
-    do {
-        showTableGameView(tableGame);
+    do
+    {
 
-        ShowOptionToSetPlayerMarkView(tableGame);
+        std::cout << "--- Menu --- " << std::endl;
+        std::cout << "1- Jugar" << std::endl;
+        std::cout << "2- Puntos" << std::endl;
+        std::cout << "3- Salir" << std::endl;
+        std::cin >> opcion;
 
-        setPlayerMarkView(tableGame, player.mark);
 
-        showTableGameView(tableGame);
-
-        if (determinatePlayerWin(tableGame, player)) {
-            showWinner(gameOver, isThereWinner, "El jugador es el ganador");
+        switch (opcion)
+        {
+        case 1: 
+            playGame(tableGame, player, npc);
+            break;
+        case 2:
+            break;
+        case 3:
+            std::cout << "Adios! hasta la proxima!" << std::endl;
+            endGame = true;
+            break;
+        default:
+                std::cout << "La opcion no es valida. Vuelve a intentarlo" << std::endl;
+            break;
         }
-        else {
-
-            if(!determinateDraw(tableGame, gameOver, isThereWinner)) {
-                setNpcMarkView(tableGame, npc.mark);
-                showTableGameView(tableGame);
-
-                if (determinateNPCWin(tableGame, npc)) {
-                    showWinner(gameOver, isThereWinner, "El NPC es el ganador");
-                }
-            }
-           
-
-        }
-
-    } while (!gameOver);
 
 
-    
+    } while (!endGame);
 }
