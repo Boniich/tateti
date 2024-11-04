a0db9e0971a716b2f3444de2435f15ec8fe34be0
Boniich <ezequieldbo25@gmail.com>
2024-11-04 11:09:50 -0300

    fix: fix a include link

--- Tateti/game/menu/Menu.h
+++ Tateti/game/menu/Menu.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <iostream>
-#include "../view/PlayGame.h"
+#include "../coreGame/PlayGame.h"
 #include "../view/ShowPointers.h"
 
 void menu(char tableGame[3][3],Player player, Player npc);
