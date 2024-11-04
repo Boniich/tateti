26dbfefbc8fc39fc80da01b34a74a673924cf3d7
Boniich <ezequieldbo25@gmail.com>
2024-11-04 11:10:33 -0300

    feat: clean the main and only keep a funtion that start the game

--- Tateti/Tateti.cpp
+++ Tateti/Tateti.cpp
@@ -1,62 +1,7 @@
 ﻿#include <iostream>
-#include <windows.h>
-#include "game/view/SelectMarkView.h"
-#include "game/view/PlayGame.h"
-#include "game/view/ShowPointers.h"
-
-
-
-
+#include "game/coreGame/StartGame.h"
 
 int main()
 {
-
-    SetConsoleOutputCP(CP_UTF8);
-
-
-    bool endGame = false;
-    int opcion;
-    Player player;
-    Player npc;
-    char tableGame[3][3] = { {' ',' ',' '}
-                            ,{' ',' ',' '}
-                            ,{' ',' ',' '} };
-
-    selectMarkView(player, npc);
-
-    std::cout << "El player tiene la marca: " << player.mark << std::endl;
-    std::cout << "El npc tiene la marca: " << npc.mark << std::endl;
-
-    do
-    {
-
-        std::cout << "--- Menu --- " << std::endl;
-        std::cout << "1- Jugar" << std::endl;
-        std::cout << "2- Puntos" << std::endl;
-        std::cout << "3- Salir" << std::endl;
-        std::cin >> opcion;
-
-
-        switch (opcion)
-        {
-        case 1: 
-            system("cls");
-            playGame(tableGame, player, npc);
-            break;
-        case 2:
-            system("cls");
-            showPointers(player, npc);
-            std::cout << "" << std::endl;
-            break;
-        case 3:
-            std::cout << "Adios! hasta la proxima!" << std::endl;
-            endGame = true;
-            break;
-        default:
-                std::cout << "La opcion no es valida. Vuelve a intentarlo" << std::endl;
-            break;
-        }
-
-
-    } while (!endGame);
+    startGame();
 }
